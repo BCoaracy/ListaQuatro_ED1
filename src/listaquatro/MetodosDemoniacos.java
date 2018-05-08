a5c678d984e20c743e23d8aeff753ccf35d2cbce
bruno <bruno.coaracy@gmail.com>
2018-05-08 08:30:08 -0300

    inicio exercicio 1

--- src/listaquatro/MetodosDemoniacos.java
+++ src/listaquatro/MetodosDemoniacos.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package listaquatro;
 
-/**
- *
- * @author marcos
- */
-public class MetodosDemoniacos {
+import java.util.Scanner;
+
+public class MetodosDemoniacos extends ListaEncadeada{
+   
+    Scanner scan = new Scanner(System.in);
+    
+    public void exercicio1(){
+        boolean s = true;
+        while(s==true){
+            System.out.println("Infomer a prioridade: ");
+            int prioridade = scan.nextInt();
+            scan.nextLine();
+            System.out.print("Informe o objeto, que vai ser uma string qualquer: ");
+            String elemento = scan.nextLine();
+            adiciona(prioridade, elemento);
+            System.out.println("Digite 1 para continuar: ");
+            if(scan.nextInt()!=1){
+                s=false;
+            }
+        }
+    }
     
 }
--- src/listaquatro/No.java
+++ src/listaquatro/No.java
@@ -2,7 +2,12 @@ package listaquatro;
 public class No {
     private No proximo;
     private Object elemento;
+    private int prioridade;
 
+    public No (int prioridade, Object elemento){
+        this.prioridade = prioridade;
+        this.elemento = elemento;
+    }
     
     public No (No proximo, Object elemento){
         this.proximo = proximo;
@@ -29,4 +34,11 @@ public class No {
         this.elemento = elemento;
     }
     
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
 }
