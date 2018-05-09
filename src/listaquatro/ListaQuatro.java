05ad44cf938f058af6d737c414ce813354b781d1
Bruno Coaracy <bruno.coaracy@gmail.com>
2018-05-09 11:42:56 -0300

    Update do metodo main
    Apagado os linhas adicionadas anteriormente, para que possa ser feito o commit com as alterações necessárias.

--- src/listaquatro/ListaQuatro.java
+++ src/listaquatro/ListaQuatro.java
@@ -7,17 +7,7 @@ public class ListaQuatro {
 
     
     public static void main(String[] args) {
-        MetodosDemoniacos md = new MetodosDemoniacos();
-        boolean s = true;
-        Scanner scan = new Scanner(System.in);
         
-        while(s==true){
-            md.exercicio1();
-            System.out.println("Informe 0 para continuar: ");
-            if(scan.nextInt()!=0){
-                s = false;
-            }
-        }
     }
     
 }
